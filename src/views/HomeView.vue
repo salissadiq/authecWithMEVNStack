b10c10c7adb7a9752fe2c17d08feda0db1807552
salissadiq <salissadiq@gmail.com>
2022-09-22 13:27:34 +0100

    Install and setup tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2 class="text-5xl">Hello world</h2>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
